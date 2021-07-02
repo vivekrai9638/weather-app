f5d1d8b35f91c34ef3103df3961a93e69458cd76
vivekrai9638 <kumarvivek8290@gmail.com>
2021-07-02 15:49:07 +0530

    final touch

--- src/app.js
+++ src/app.js
@@ -14,12 +14,20 @@ const partialPath = path.join(__dirname, "../templates/partials");
 
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
+
 hbs.registerPartials(partialPath);
 
 app.use(express.static(publicDirectoryPath));
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/help", (req, res) => {
+  res.render("help");
 });
 
 app.get("/weather", (req, res) => {
